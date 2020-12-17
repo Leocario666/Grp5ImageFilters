06d9d611a50b5c95e86b1ea50f6d5d741a0e5c9b
Alexandre Piguet <48336551+aphevs@users.noreply.github.com>
2020-12-17 14:20:27 +0100

    InOutPutFile and Edge/ImageFilters class

---
+++ Business Layer/Matrix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer
+{
+    public static class Matrix
+    {
+        public static double[,] Laplacian3x3
+        {
+            get
+            {
+                return new double[,]
+                { { -1, -1, -1,  },
+                  { -1,  8, -1,  },
+                  { -1, -1, -1,  }, };
+            }
+        }
+
+        public static double[,] Laplacian5x5
+        {
+            get
+            {
+                return new double[,]
+                { { -1, -1, -1, -1, -1, },
+                  { -1, -1, -1, -1, -1, },
+                  { -1, -1, 24, -1, -1, },
+                  { -1, -1, -1, -1, -1, },
+                  { -1, -1, -1, -1, -1  }, };
+            }
+        }
+
+        public static double[,] LaplacianOfGaussian
+        {
+            get
+            {
+                return new double[,]
+                { {  0,   0, -1,  0,  0 },
+                  {  0,  -1, -2, -1,  0 },
+                  { -1,  -2, 16, -2, -1 },
+                  {  0,  -1, -2, -1,  0 },
+                  {  0,   0, -1,  0,  0 }, };
+            }
+        }
+
+    }
+}
---
+++ Input Output Files/InOutPutFile.cs
@@ -0,0 +1,69 @@
+﻿using Business_Layer;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Input_Output_Files
+{
+    public class InOutPutFile
+    {
+        public Bitmap loadImage()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select an image file.";
+            ofd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+            ofd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader streamReader = new StreamReader(ofd.FileName);
+                Bitmap originalBitmap = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+                streamReader.Close();
+                
+
+               Bitmap previewBitmap = originalBitmap.CopyToSquareCanvas(600);
+                //picPreview.Image = previewBitmap;
+
+                return previewBitmap;
+            }
+            return null;
+        }
+
+
+
+        public void saveImage(Bitmap image)
+        {
+            if (image != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Specify a file name and file path";
+                sfd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+                sfd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string fileExtension = Path.GetExtension(sfd.FileName).ToUpper();
+                    ImageFormat imgFormat = ImageFormat.Png;
+
+                    if (fileExtension == "BMP")
+                    {
+                        imgFormat = ImageFormat.Bmp;
+                    }
+                    else if (fileExtension == "JPG")
+                    {
+                        imgFormat = ImageFormat.Jpeg;
+                    }
+
+
+                    StreamWriter streamWriter = new StreamWriter(sfd.FileName, false);
+
+                    //Save the final image (with any filters applied)
+                    image.Save(streamWriter.BaseStream, imgFormat);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+            }
+        }
+    }
+}
