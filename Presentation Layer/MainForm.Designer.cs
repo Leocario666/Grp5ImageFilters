c6610d8d2c213f1094532aa5b2ddde2ee78f07d9
Alexandre Piguet <48336551+aphevs@users.noreply.github.com>
2020-12-17 13:33:12 +0100

    Save button

--- Presentation Layer/MainForm.Designer.cs
+++ Presentation Layer/MainForm.Designer.cs
@@ -29,24 +29,39 @@
         private void InitializeComponent()
         {
             this.buttonLoad = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonLoad
             // 
-            this.buttonLoad.Location = new System.Drawing.Point(33, 448);
+            this.buttonLoad.Location = new System.Drawing.Point(66, 862);
+            this.buttonLoad.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonLoad.Name = "buttonLoad";
-            this.buttonLoad.Size = new System.Drawing.Size(89, 31);
+            this.buttonLoad.Size = new System.Drawing.Size(178, 60);
             this.buttonLoad.TabIndex = 0;
             this.buttonLoad.Text = "Load Image";
             this.buttonLoad.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(884, 862);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonLoad);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "MainForm";
-            this.Size = new System.Drawing.Size(623, 611);
+            this.Size = new System.Drawing.Size(1246, 1175);
             this.ResumeLayout(false);
 
         }
@@ -54,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Presentation Layer/MainForm.cs
+++ Presentation Layer/MainForm.cs
@@ -16,5 +16,10 @@ namespace Presentation_Layer
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
